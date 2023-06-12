dca87b920f53408e79b2ead305e186e23b927d9c
EmmanuelC1 <emcastillo@csumb.edu>
2023-06-11 19:09:55 -0700

    separates order into new component, and conditionally renders

--- src/Components/Pizza.jsx
+++ src/Components/Pizza.jsx
@@ -1,13 +1,13 @@
 export default function Pizza({ pizzaObj }) {
-  const { name, photoName, ingredients, price } = pizzaObj;
+  const { name, photoName, ingredients, price, soldOut } = pizzaObj;
 
   return (
-    <li className="pizza">
+    <li className={`pizza ${soldOut ? 'sold-out' : ''}`}>
       <img src={photoName} alt={name} />
       <div>
         <h3>{name}</h3>
         <p>{ingredients}</p>
-        <span>{price}</span>
+        <span>{soldOut ? 'SOLD OUT' : price}</span>
       </div>
     </li>
   );
